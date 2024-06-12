5d658d791cc6967ff3151072e2dcfd93f43b28e1
Derek Myers <cashmerelc@gmail.com>
2024-06-12 15:35:08 +0200

    Update LeagueTeam schema to include the real world european club and removed unnecessary commented code from league details page

--- src/db/models/LeagueTeam.mjs
+++ src/db/models/LeagueTeam.mjs
@@ -6,6 +6,7 @@ const leagueTeamSchema = new Schema({
   name: { type: String, required: true },
   leagueId: { type: String, required: true },
   userId: { type: String, required: true },
+  euroClub: { type: String, required: true },
 });
 
 const LeagueTeam =
